5edb718aac671e17f9f8356b004fe6fdbf1ef135
imohashi <leonardoohashi.mkt@gmail.com>
2021-03-24 13:27:31 -0300

    edited error message

--- src/errors/database-error.ts
+++ src/errors/database-error.ts
@@ -2,7 +2,7 @@ import { NativeError } from "mongoose";
 
 export class DatabaseError extends Error {
   constructor(paramName: NativeError) {
-    super(`Database not connected: ${paramName}`);
+    super(`Database not is connected\n ${paramName}`);
     this.name = "DatabaseError";
   }
 }
