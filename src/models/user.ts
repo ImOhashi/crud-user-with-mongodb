93156fe60b6457166d3abe0b5af9ddbe0e301371
imohashi <leonardoohashi.mkt@gmail.com>
2021-03-26 11:18:56 -0300

    add a config in users schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,20 +2,27 @@ import { model, Model, Schema } from "mongoose";
 
 import { User } from "../interfaces";
 
-const UserSchema: Schema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const UserSchema: Schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: false,
+    collection: "users",
+    id: false
+  }
+);
 
 const UserModel: Model<User> = model("User", UserSchema);
 
