e52c7c683a8b857d3e979258fe690409943c4dcb
imohashi <leonardoohashi.mkt@gmail.com>
2021-03-26 14:07:32 -0300

    User service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,29 +1,30 @@
+import UserRepository from "../repositories/userRepository";
 import { IUserRepository, IUserService, User } from "../interfaces";
 
 export default class UserService implements IUserService {
-  private userRepository: IUserRepository;
+  private _userRepository: IUserRepository;
 
-  constructor(userRepository: IUserRepository) {
-    this.userRepository = userRepository;
+  constructor(userRepository: UserRepository) {
+    this._userRepository = userRepository;
   }
 
   public async getUser(userId: string): Promise<User> {
-    return this.userRepository.getUser(userId);
+    return this._userRepository.getUser(userId);
   }
 
   public async getUsers(userIds?: string[]): Promise<User[]> {
-    return this.userRepository.getUsers(userIds);
+    return this._userRepository.getUsers(userIds);
   }
 
   public async createUser(user: User): Promise<User> {
-    return this.userRepository.createUser(user);
+    return this._userRepository.createUser(user);
   }
 
   public async updateUser(userId: string, user: User): Promise<User> {
-    return this.userRepository.updateUser(userId, user);
+    return this._userRepository.updateUser(userId, user);
   }
 
   public async deleteUser(userId: string): Promise<User> {
-    return this.userRepository.deleteUser(userId);
+    return this._userRepository.deleteUser(userId);
   }
 }
