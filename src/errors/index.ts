972cd53c274dae974b764f7f2fb8ab3a033ef69f
imohashi <leonardoohashi.mkt@gmail.com>
2021-03-26 11:07:16 -0300

    add a new http helper

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,1 +1,2 @@
 export * from "./database-error";
+export * from "./server-error";
---
+++ src/helpers/httpHelper.ts
@@ -0,0 +1,27 @@
+import { ServerError } from "../errors";
+import { HttpResponse } from "../protocols";
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error,
+});
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack),
+});
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data,
+});
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null,
+});
